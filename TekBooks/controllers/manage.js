5036bd0750e55613e7eea00404ff9d0908e5f12c
Benjamin Ubach <bubach@homeaway.com>
2017-09-07 00:13:22 -0500

    Edit/Delete books

--- TekBooks/controllers/manage.js
+++ TekBooks/controllers/manage.js
@@ -26,37 +26,85 @@ module.exports = function(router){
     })
 
     router.post('/books', (req, res) => {
-        const {title, category, author, 
-            publisher, price, description, 
-            cover} = req.body
-            if(title === '' || price === ''){
-                req.flash('error', 'Please fill out required fields')
-                res.location('/manage/books/add')
-                return res.redirect('/manage/books/add')
-            }
-            if(isNaN(price)){
-                req.flash('error', 'Please fill out required fields')
-                res.location('/manage/books/add')
-                return res.redirect('/manage/books/add')
+        const {title, category, author, publisher, price, description, cover} = req.body
+        if(title === '' || price === ''){
+            req.flash('error', 'Please fill out required fields')
+            res.location('/manage/books/add')
+            return res.redirect('/manage/books/add')
+        }
+        if(isNaN(price)){
+            req.flash('error', 'Please fill out required fields')
+            res.location('/manage/books/add')
+            return res.redirect('/manage/books/add')
 
-            }
-            const newBook = new Book({title, category, author, 
+        }
+        const newBook = new Book({title, category, author, 
             publisher, price, description, cover})
 
-            newBook.save((err) => {
+        newBook.save((err) => {
+            if(err){
+                console.error(err);
+            }
+            console.log("Locals B4", res.locals);
+            req.flash('success', 'Book Created!')
+            console.log("Locals After", res.locals);
+            res.location('/manage/books')
+            return res.redirect('/manage/books')
+        })
+    })
+
+    router.get('/books/edit/:_id', (req, res) =>{
+        let {_id} = req.params
+        console.log("edit")
+        Category.find({}, (err, categories) =>{
+            Book.findOne({_id}, (err, book) => {
+                if(err){
+                    console.log(err)
+                }
+                res.render('manage/books/edit', {categories, book})
+            })
+        })
+    })
+
+    router.post('/books/edit/:_id', (req, res) =>{
+        const { title, category, author, publisher, price, description, cover} = req.body
+        const {_id} = req.params
+        if(title === '' || price === ''){
+            req.flash('error', 'Please fill out required fields')
+            res.location(`/manage/books/edit/${_id}`)
+            return res.redirect(`/manage/books/edit/${_id}`)
+        }
+        if(isNaN(price)){
+            req.flash('error', 'Please fill out required fields')
+            res.location(`/manage/books/edit/${_id}`)
+            return res.redirect(`/manage/books/edit/${_id}`)
+
+        }
+        Book.update({_id}, { title, category, author, 
+            publisher, price, description, 
+            cover}, (err, book) => {
                 if(err){
                     console.error(err);
                 }
-                console.log("Locals B4", res.locals);
-                req.flash('success', 'Book Created!')
-                console.log("Locals After", res.locals);
+                req.flash('success', "Book Updated")
                 res.location('/manage/books')
-                return res.redirect('/manage/books')
+                res.redirect('/manage/books')
             })
+    })
+
+    router.post('/books/delete/:_id', (req, res) =>{
+        const {_id} = req.params
+        Book.remove({_id}, (err) => {
+            if(err){
+                return console.err(err)
+            }
+            req.flash("success", 'Book Deleted')
+            res.location('/manage/books')
+            res.redirect('/manage/books')
         })
+    })
 
     router.get('/categories', (req, res) => {
         res.render('manage/categories/index')
     })
-    
 }
